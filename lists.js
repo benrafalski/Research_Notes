cc1b436f3abfe6efc07a4800e03bba972497a906
benrafalski <79751177+benrafalski@users.noreply.github.com>
2024-08-03 18:00:01 -0700

    russia

--- lists.js
+++ lists.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
     // jQuery methods go here...
-    $(".list-content-body").hide()
+    $(".list-content-body").slideUp(5)
 
     $(".list-content-title").click(function(){
         var list = $(this).next()
         
         if(list.attr('class') == 'list-content-body'){
             list.slideToggle()
+            // list.show()
         }
         $(this).find('i').toggleClass('active');
     })
