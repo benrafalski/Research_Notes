d28889b5b8dfd2805d0dfc5afea6b16558d01ac6
benrafalski <brafalsk@asu.edu>
2024-09-30 18:13:28 -0700

    russian

--- lists.js
+++ lists.js
@@ -7,29 +7,32 @@ $(document).ready(function(){
 
     $(".list-content-container.inactive").find('.list-content-body').slideUp(5);
     // $(".list-content-container.inactive").find('i.fa.fa-chevron-right').toggleClass('active');
-    $(".list-content-title").click(function(){
-        var list = $(this).next()
+    $(".list-content-title > i").click(function(){
+        console.log("here")
+        // var list = $(this).next()
+        var list = $(this).parent().next()
 
         console.log(list.attr('class'))
         
         if(list.attr('class') == 'list-content-body'){
             list.slideToggle()
         }
-        $(this).find('i').toggleClass('active');
+        $(this).parent().find('i').toggleClass('active');
     })
 
 
 
     $(".sidenav-dropdown-container.inactive").find('.sidenav-sublist').slideUp(5);
     // $(".sidenav-dropdown-container.inactive").find('i.fa.fa-chevron-right').toggleClass('active');
-    $(".sidenav-dropdown-title").click(function(){
-        var list = $(this).next()
+    $(".sidenav-dropdown-title > i").click(function(){
+
+        var list = $(this).parent().next()
 
         console.log(list.attr('class'))
         
         if(list.attr('class') == 'sidenav-sublist'){
             list.slideToggle()
         }
-        $(this).find('i').toggleClass('active');
+        $(this).parent().find('i').toggleClass('active');
     })
 });
